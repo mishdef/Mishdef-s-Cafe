26836f935f860395e71c0c5645b58db80af0da64
Michailo Stadnikov <stadnmisha@gmail.com>
2025-07-02 19:55:25 +0300

    Functions for the menu item "1.Add item" have been written. This menu item is now fully functional

--- Mishdef's Cafe/Program.cs
+++ Mishdef's Cafe/Program.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static MyFunctions.Tools;
+using static MyFunctions.MessageBox;
+using MyFunctions;
 
 namespace Mishdef_s_Cafe
 {
     internal class Program
     {
+        public static string[][] itemsAndCost = new string[2][]
+            {
+            new string[] {},
+            new string[] {},
+            };
+
         static void Main(string[] args)
         {
+
             Console.OutputEncoding = Encoding.UTF8;
             do
             {
@@ -37,6 +46,7 @@ namespace Mishdef_s_Cafe
                     {
                         case 1:
                             {
+                                MenuAddItem();
                                 break;
                             }
                         case 2:
@@ -80,6 +90,50 @@ namespace Mishdef_s_Cafe
                     Console.WriteLine("An error occurred: " + ex.Message);
                 }
             } while (true);
+        }
+
+        static void MenuAddItem()
+        {
+            if (itemsAndCost[0].Length >= 5)
+            {
+                MessageBox.Show("You cannot add more than 5 items.", "Error", Buttons.Ok);
+                return;
+            }
+
+            Console.Clear();
+
+            string itemName;
+            do
+            {
+                Console.Write("Enter item name: ");
+                itemName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(itemName) || itemName.Length < 3 || itemName.Length > 25)
+                {
+                    Console.WriteLine("Item name must be between 3 and 25 characters long. Please try again.");
+                }
+            } while (string.IsNullOrWhiteSpace(itemName) || itemName.Length < 3 || itemName.Length > 20);
+            double itemCost = InputDouble("Enter item cost: ", InputType.Without, 0);
+
+            MessageBox.Show(AddItem(itemName, itemCost),"Message", Buttons.Ok);
+        }
+
+        static string AddItem(string itemName, double itemCost)
+        {
+            try
+            {
+                int currentItemsCount = itemsAndCost[0].Length;
+                Array.Resize(ref itemsAndCost[0], currentItemsCount + 1);
+                Array.Resize(ref itemsAndCost[1], currentItemsCount + 1);
+
+                itemsAndCost[0][currentItemsCount] = itemName;
+                itemsAndCost[1][currentItemsCount] = itemCost.ToString("F2");
+
+                return $"Item '{itemName}' with cost {itemCost:F2}$ added successfully.";
+            }
+            catch (Exception ex)
+            {
+                return "An error occurred while adding the item: " + ex.Message;
+            }
         }
     }
 }
